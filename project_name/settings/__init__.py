8eaa848c6708f3d262a5fca49ef1368c643dbac6
Igor Gai <igor.gai@i.ua>
2015-05-05 14:44:04 +0300

    modular settings

---
+++ project_name/settings/__init__.py
@@ -0,0 +1,3 @@
+# settings module itself should return local settings so that
+# manage.py commands work.
+from .local import *
---
+++ project_name/settings/local.example.py
@@ -0,0 +1,42 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ''
+
+DEBUG = True
+THUMBNAIL_DEBUG = DEBUG
+
+# COMPRESS_ENABLED = True
+
+# ALLOWED_HOSTS = ['127.0.0.1']
+
+INSTALLED_APPS += (
+    # 'django-debug-toolbar',
+)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+        'HOST': 'localhost',
+        'USER': '',
+        'PASSWORD': '',
+        'PORT': ''
+    }
+}
+
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'your-username@gmail.com'
+EMAIL_HOST_PASSWORD = 'your-password'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+#         'LOCATION': '127.0.0.1:11211',
+#     }
+# }
