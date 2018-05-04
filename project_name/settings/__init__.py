00ab1cf9e7ee001b332b820695b7b9799fdb1a0f
Igor Gai <i@igorgai.com>
2018-05-05 00:32:52 +0300

    Catch missing local settings file exception in Python 3 style

--- project_name/settings/__init__.py
+++ project_name/settings/__init__.py
@@ -4,10 +4,9 @@ try:
     from .local import *
 except ImportError as e:
     settings_path = '{{ project_name }}/settings'
-    error_message = '{mes} (did you copy {path}/local.example.py to {path}/local.py?)'.format(
-        mes=e.args[0],
-        path=settings_path
+    error_message = '{orig_msg}\n\n\t(Have you copied {path}/local.example.py to {path}/local.py?)\n'.format(
+        orig_msg=e.args[0],
+        path=settings_path,
     )
-    e.args = tuple([error_message])
 
-    raise e
+    raise type(e)(error_message) from e
