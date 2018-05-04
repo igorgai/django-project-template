fa9753c6e4d2e0529772b44bb263103e8dbfd9c2
Igor Gai <i@igorgai.com>
2018-05-05 00:52:56 +0300

    Add dummy index page just as an example; rename 'parts' directory to 'partials'

--- project_name/urls.py
+++ project_name/urls.py
@@ -17,10 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     # url(r'^accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
+
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
 ]
 
 if settings.DEBUG:
